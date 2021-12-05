fa7bd73d37b501cd93899d04dcba80cb1d7b0662
pierslabs <pedrolosasp@gmail.com>
2021-12-05 18:50:06 +0100

    .eslintrc.json

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
   images:{
     domains:['i.annihil.us']
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
  
 }
