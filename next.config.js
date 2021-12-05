8bb4f34a98d7129d917987ce3883cfec04eff3be
pierslabs <pedrolosasp@gmail.com>
2021-12-05 18:55:46 +0100

    .eslintrc.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const securityHeaders = []
 module.exports = {
-  
+  async headers() {
+    return [
+      {
+        
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+  reactStrictMode: true,
   images:{
     domains:['i.annihil.us']
   },
