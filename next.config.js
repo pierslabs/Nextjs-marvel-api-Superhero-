1e694a68251ffb4434e62b69473015a6fb5f8de9
pierslabs <pedrolosasp@gmail.com>
2021-12-05 18:53:27 +0100

    .eslintrc.json

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-const securityHeaders = []
-
 module.exports = {
-  reactStrictMode: true,
+  
   images:{
     domains:['i.annihil.us']
   },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ]
-  }
  
 }
